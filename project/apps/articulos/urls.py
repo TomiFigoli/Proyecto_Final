cdf4b462ddcbcd7ea2b72a1a34c3c5118b0cc636
Tomas Figoli <tfigoli1@gmail.com>
2023-06-12 23:49:43 -0300

    intentos

--- project/apps/articulos/urls.py
+++ project/apps/articulos/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="articulos/index.html"), name="index"),
     path("articuloscategoria/create/", views.ArticulosCategoriaCreate.as_view(), name="articulos_categoria_create"),
     path("articulos/create", views.ArticulosCreate.as_view(), name="articulos_create"),
+    path("articuloscategoria/list", views.ArticuloCategoriaList.as_view(), name="articuloscategoria_list"),
+    path("articulos/detail", views.ArticuloDetail.as_view(), name="articulos_detail"),
+    path("articuloscategoria/detail", views.ArticuloCategoriaDetail.as_view(), name="articulos_categoria_detail")
 ]
--- project/apps/articulos/views.py
+++ project/apps/articulos/views.py
@@ -7,15 +7,35 @@ from . import forms
 
 
 # Create your views here.
-
+# categorias
 class ArticulosCategoriaCreate(CreateView):
     model = models.ArticulosCategoria
     form_class = forms.ArticulosCategoriaForm
     success_url = reverse_lazy("articulos:index")
 
+class ArticuloCategoriaList(ListView):
+    model = models.ArticulosCategoria
+
+    def get_queryset(self):
+        if self.request.GET.get("consulta"):
+            query = self.request.GET.get("consulta")
+            object_list = models.ArticulosCategoria.objects.filter(nombre__icontains=query)
+        else:
+            object_list = models.ArticulosCategoria.objects.all()
+        return object_list
+
+class ArticuloCategoriaDetail(DetailView):
+    model = models.ArticulosCategoria
+# articulos
+
 class ArticulosCreate(CreateView):
     model = models.Articulos
     form_class = forms.ArticulosForm
     success_url = reverse_lazy("articulos:index")
+
+class ArticuloDetail(DetailView):
+    model = models.Articulos
+
+
     
 
